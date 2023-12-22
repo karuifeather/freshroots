1fc5ff1d1f9d111377991d9648d63866e622d445
Aashaya Aryal <aryalaashaya@gmail.com>
2023-12-22 12:34:44 -0600

    some customization

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.html'],
+  content: ['./public/**/*.html'],
   theme: {
+    extend: {
+      colors: {
+        'fr-primary-green': '#4CAF50',
+        'fr-accent-green': '#8BC34A',
+        'fr-earthy-brown': '#795548',
+        'fr-sunny-yellow': '#FFC107',
+        'fr-soft-grey': '#9E9E9E',
+        'fr-clean-white': '#FFFFFF',
+        'fr-deep-grey': '#607D8B',
+      },
+    },
+    fontFamily: {
+      Lato: ['Lato, sans-serif'],
+      Raleway: ['Raleway, sans-serif'],
+    },
     container: {
       center: true,
+      padding: '1rem',
+      screens: {
+        lg: '1124px',
+        xl: '1124px',
+        '2xl': '1124px',
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
